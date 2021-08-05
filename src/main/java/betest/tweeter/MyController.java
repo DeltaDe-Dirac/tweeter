f7deb2abee0762c16b0ed14974a675b4b7fbbfda
Delta de Dirac <igorshevtsov@gmail.com>
2021-08-05 19:14:43 +0300

    add query for custom retweet

--- src/main/java/betest/tweeter/MyController.java
+++ src/main/java/betest/tweeter/MyController.java
@@ -5,23 +5,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import betest.tweeter.entities.Like;
+import betest.tweeter.entities.custom.IRetweetCust;
 import betest.tweeter.entities.custom.ITweetCust;
 import betest.tweeter.repos.LikeRepository;
+import betest.tweeter.repos.ReTweetRepository;
 import betest.tweeter.repos.TweetRepository;
 
 @RestController
 public class MyController {
 	private final TweetRepository tweetRepo;
+	private final ReTweetRepository reTweetRepo;
 	private final LikeRepository likeRepo;
 
-	MyController(TweetRepository tweetRepo, LikeRepository likeRepo) {
+	MyController(TweetRepository tweetRepo, LikeRepository likeRepo, ReTweetRepository reTweetRepo) {
 		this.tweetRepo = tweetRepo;
+		this.reTweetRepo = reTweetRepo;
 		this.likeRepo = likeRepo;
 	}
 	
 	@GetMapping("/tweets")
 	public List<ITweetCust> custTweets() {
 		return tweetRepo.getTweetCustNative();
+	}
+	
+	@GetMapping("/retweets")
+	public List<IRetweetCust> custRetweets() {
+		return reTweetRepo.getRetweetCustNative();
 	}
 	
 	@GetMapping("/likes")
--- src/main/java/betest/tweeter/repos/ReTweetRepository.java
+++ src/main/java/betest/tweeter/repos/ReTweetRepository.java
@@ -1,8 +1,13 @@
 package betest.tweeter.repos;
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import betest.tweeter.entities.ReTweet;
+import betest.tweeter.entities.custom.IRetweetCust;
 
 public interface ReTweetRepository extends JpaRepository<ReTweet, Integer> {
-	
+	@Query(value = "SELECT T.TEXT_CONTENT AS CONTENT, R.USERNAME AS RETWEET_USER, T.ID AS TWEET_ID, T.USERNAME AS TWEET_USER, R.TIMESTAMP FROM TWEETS AS T "
+			+ "LEFT JOIN RETWEETS R ON T.ID = R.POSTID", nativeQuery = true)
+	List<IRetweetCust> getRetweetCustNative();
 }
-
