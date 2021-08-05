ec641a660024417a3bc4007798cc99da19adbeee
Delta de Dirac <igorshevtsov@gmail.com>
2021-08-05 19:08:25 +0300

    fix table name for consistency

--- src/main/java/betest/tweeter/repos/TweetRepository.java
+++ src/main/java/betest/tweeter/repos/TweetRepository.java
@@ -8,7 +8,7 @@ import betest.tweeter.entities.custom.ITweetCust;
 import java.util.List;
 
 public interface TweetRepository extends JpaRepository<Tweet, Integer> {
-	@Query(value = "SELECT J.*, COUNT(R.POSTID) AS RETWEETS_COUNT FROM RETWEET AS R "
+	@Query(value = "SELECT J.*, COUNT(R.POSTID) AS RETWEETS_COUNT FROM RETWEETS AS R "
 			+ "RIGHT JOIN (SELECT T.ID, TEXT_CONTENT AS CONTENT, T.TIMESTAMP, T.USERNAME, COUNT(L.ID) AS LIKES_COUNT FROM TWEETS AS T "
 			+ "LEFT JOIN LIKES AS L ON T.ID = L.POSTID GROUP BY POSTID) J ON J.ID = R.POSTID GROUP BY R.POSTID ORDER BY J.ID", nativeQuery = true)
 	List<ITweetCust> getTweetCustNative();
