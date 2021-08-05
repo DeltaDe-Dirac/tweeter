f1bc0e70cfab3a28c365909fa6cf8b03bd40dcfd
Delta de Dirac <igorshevtsov@gmail.com>
2021-08-05 15:26:53 +0300

    add temp controller to test rest response

---
+++ src/main/java/betest/tweeter/MyController.java
@@ -0,0 +1,36 @@
+package betest.tweeter;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import betest.tweeter.entities.Like;
+import betest.tweeter.entities.Tweet;
+import betest.tweeter.repos.LikeRepository;
+import betest.tweeter.repos.TweetRepository;
+
+@RestController
+public class MyController {
+	private final TweetRepository tweetRepo;
+	private final LikeRepository likeRepo;
+
+	MyController(TweetRepository tweetRepo, LikeRepository likeRepo) {
+		this.tweetRepo = tweetRepo;
+		this.likeRepo = likeRepo;
+	}
+
+	@GetMapping("/tweets")
+	public List<Tweet> allTweets() {
+		return tweetRepo.findAll();
+	}
+	
+	@GetMapping("/likes")
+	public List<Like> allLikes() {
+		return likeRepo.findAll();
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World, I am coming fully charged!";
+	}
+}
