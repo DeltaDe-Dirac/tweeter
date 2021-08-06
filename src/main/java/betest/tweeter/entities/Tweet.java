2ac0bb8ea38654b2298aa04360d9612c2243c0d6
Delta de Dirac <igorshevtsov@gmail.com>
2021-08-06 21:14:18 +0300

    imports clean up

--- src/main/java/betest/tweeter/entities/Tweet.java
+++ src/main/java/betest/tweeter/entities/Tweet.java
@@ -1,13 +1,11 @@
 package betest.tweeter.entities;
 
 import java.sql.Timestamp;
-import java.util.List;
 import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
