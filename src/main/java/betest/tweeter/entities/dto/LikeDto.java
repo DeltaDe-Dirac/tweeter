1ae6984e352248904a8526f3d8ee4062064dd323
Delta de Dirac <igorshevtsov@gmail.com>
2021-08-07 20:55:27 +0300

    fix text formatting

--- src/main/java/betest/tweeter/entities/dto/LikeDto.java
+++ src/main/java/betest/tweeter/entities/dto/LikeDto.java
@@ -6,6 +6,7 @@ public class LikeDto {
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
--- src/main/java/betest/tweeter/entities/dto/TweetDto.java
+++ src/main/java/betest/tweeter/entities/dto/TweetDto.java
@@ -7,12 +7,15 @@ public class TweetDto {
 	public String getContent() {
 		return content;
 	}
+	
 	public void setContent(String content) {
 		this.content = content;
 	}
+	
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
