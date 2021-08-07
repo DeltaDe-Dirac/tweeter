c6a1139d3fe8b78217db218603faba0cc06f2c0a
Delta de Dirac <igorshevtsov@gmail.com>
2021-08-07 14:19:47 +0300

    handle post request for new tweets

--- src/main/java/betest/tweeter/MyController.java
+++ src/main/java/betest/tweeter/MyController.java
@@ -1,12 +1,23 @@
 package betest.tweeter;
+
+import java.sql.Timestamp;
 import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import betest.tweeter.entities.Like;
+import betest.tweeter.entities.Tweet;
 import betest.tweeter.entities.custom.IRetweetCust;
 import betest.tweeter.entities.custom.ITweetCust;
+import betest.tweeter.entities.dto.TweetDto;
 import betest.tweeter.repos.LikeRepository;
 import betest.tweeter.repos.ReTweetRepository;
 import betest.tweeter.repos.TweetRepository;
@@ -17,29 +28,31 @@ public class MyController {
 	private final ReTweetRepository reTweetRepo;
 	private final LikeRepository likeRepo;
 
+	@Autowired
+	private ModelMapper modelMapper;
+
 	MyController(TweetRepository tweetRepo, LikeRepository likeRepo, ReTweetRepository reTweetRepo) {
 		this.tweetRepo = tweetRepo;
 		this.reTweetRepo = reTweetRepo;
 		this.likeRepo = likeRepo;
 	}
-	
+
 	@GetMapping("/tweets")
 	public List<ITweetCust> custTweets() {
 		return tweetRepo.getTweetCustNative();
 	}
-	
+
 	@GetMapping("/retweets")
 	public List<IRetweetCust> custRetweets() {
 		return reTweetRepo.getRetweetCustNative();
 	}
-	
-	@GetMapping("/likes")
-	public List<Like> allLikes() {
-		return likeRepo.findAll();
-	}
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World, I am coming fully charged!";
+	@PostMapping("/tweets")
+	public Tweet newTweet(@RequestBody TweetDto newTweetTdo) {
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+		modelMapper.typeMap(TweetDto.class, Tweet.class);
+		Tweet newTweet = modelMapper.map(newTweetTdo, Tweet.class);
+		newTweet.setTimestamp(new Timestamp(System.currentTimeMillis()));
+		return tweetRepo.save(newTweet);
 	}
 }
